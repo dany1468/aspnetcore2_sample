bddca29889b0b116512cdd9fb503c830ef638a38
dany1468 <dany1468@gmail.com>
2019-01-13 21:52:55 +0900

    dotnet ef dbcontext scaffold "Server=localhost,1433\\Database=TutorialDb;User=SA;Password=P@55w0rd;" Microsoft.EntityFrameworkCore.SqlServer

---
+++ src/masterContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace src
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost,1433\\Database=TutorialDb;User=SA;Password=P@55w0rd;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+        }
+    }
+}
